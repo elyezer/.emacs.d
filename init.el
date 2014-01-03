7a3a655146663455aaaa2e652d91dff4a8725cf6
Elyézer Mendes Rezende <elyezermr@gmail.com>
2014-01-03 18:19:50 -0200

    Add backup of files settings
    * Add setting to place all backup files in the backups directory
    * Add setting to make backup of files even when they're in version control

--- init.el
+++ init.el
@@ -59,6 +59,14 @@
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
 (setq exec-path (append exec-path '("/usr/local/bin")))
 
+;; Write backup files to own directory
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+                 (concat user-emacs-directory "backups")))))
+
+;; Make backups of files, even when they're in version control
+(setq vc-make-backup-files t)
+
 (require 'magit)
 (global-set-key "\C-xg" 'magit-status)
 
